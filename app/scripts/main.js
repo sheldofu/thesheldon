f347561e0adccd430f5a7f21aafc74c1b8420b62
Sheldon Marsh <thesheldon@googlemail.com>
2015-07-15 08:17:00 +0100

    working on blog

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
-console.log('\'Allo \'Allo!');
+angular.module('sheldoBlog', [])
+.controller('Retrieve', function($scope, $http) {
+    $http.get('https://blogapi.herokuapp.com/blog/all').success(function(data) {
+            $scope.posts = data;
+        });
+});
+
+/**
+* Configure the Routes
+*/
+app.config(['$routeProvider', function ($routeProvider) {
+$routeProvider
+.when("/", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
+.when("/blog/post", {templateUrl: "partials/blog_item.html",controller: "BlogCtrl"})
+.otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+}]);
