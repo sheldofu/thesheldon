8ef62100afe4fe05c8633cd450c46c1e55157afc
Sheldon Marsh <thesheldon@googlemail.com>
2015-07-15 21:06:39 +0100

    angular routing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,20 @@
-angular.module('sheldoBlog', [])
+var app = angular.module('sheldoBlog', ['ngRoute'])
 .controller('Retrieve', function($scope, $http) {
     $http.get('https://blogapi.herokuapp.com/blog/all').success(function(data) {
             $scope.posts = data;
         });
 });
 
+app.controller('BlogCtrl',function($scope, $http) {
+	// console.log('control');
+})
+
 /**
 * Configure the Routes
 */
 app.config(['$routeProvider', function ($routeProvider) {
 $routeProvider
-.when("/", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
-.when("/blog/post", {templateUrl: "partials/blog_item.html",controller: "BlogCtrl"})
-.otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+.when("/", {templateUrl: "partials/blog_all.html", controller: "BlogCtrl"})
+.when("/post", {templateUrl: "partials/blog_item.html",controller: "BlogCtrl"})
+// .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
